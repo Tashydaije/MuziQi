467fe5476dbc6462e0a7fa329a8171be906b9f6e
Tashydaije <mbuguanatasha01@gmail.com>
2024-08-15 10:53:30 +0300

    setting up the server

---
+++ backend-MuziQi/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import { connectToServer } from './src/config/db.js';
+import dotenv from 'dotenv';
+import authRoutes from './src/routes/auth.js';
+
+dotenv.config();
+
+const app = express();
+
+// Connect to MongoDB
+connectToServer();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
