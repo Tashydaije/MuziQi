995f1cd4eba0d65cfbcbb4acdf707aaa7ffc44a7
Tashydaije <mbuguanatasha01@gmail.com>
2024-08-20 22:14:16 +0300

    package.json file

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,6 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/playlists', playlistRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
