8e02d91b9e99408b4919e913a135aa2515b756f6
Salome-N <sallynjenga8@gmail.com>
2024-08-21 19:13:13 +0300

    Basic Home and Not Found page

---
+++ frontend/babel.config.js
@@ -0,0 +1,8 @@
+module.export = {
+  "plugins": [
+    "@babel/plugin-transform-class-properties",
+    "@babel/plugin-transform-optional-chaining",
+    "@babel/plugin-transform-nullish-coalescing-operator",
+    "@babel/plugin-transform-private-methods"
+  ]
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux'; // Import Provider to connect Redux
-import './index.css';
-import store from './redux/store'; // Import the Redux store
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+import { ToastContainer } from "react-toastify";
+import store from "./redux/store";
+import App from "./App";
+import "react-toastify/dist/ReactToastify.css";
+import "./global.css";
 
+let persistor = persistStore(store);
 
-// Render the app and wrap it with the Redux Provider
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<PersistGate persistor={persistor}>
+				<BrowserRouter>
+					<App />
+					<ToastContainer
+						position="bottom-center"
+						autoClose={2000}
+						hideProgressBar={true}
+						closeButton={false}
+						theme="colored"
+						icon={false}
+					/>
+				</BrowserRouter>
+			</PersistGate>
+		</Provider>
+	</React.StrictMode>,
 );
