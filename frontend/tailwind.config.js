e32a27822d1c34a6b96d70eeaa427ccf4beeca0a
OjElsa <elsahojwang06@gmail.com>
2024-08-23 22:30:17 +0300

    tailwind packages

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Make sure Tailwind scans all your files for classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
