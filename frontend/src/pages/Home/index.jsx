4d7124256f5880e46c9fed7aced1d32db5659db5
Salome-N <sallynjenga8@gmail.com>
2024-08-24 02:36:05 +0300

    Update few changes

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import Navbar from '../../components/Navbar';
+import Layout from '../../components/Layout';
+import NewIn from '../../components/NewIn';
 import styles from './Home.module.scss';
 
 const Home = () => {
   return (
-    <div className={styles.homeContainer}>
-      <Navbar />
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the main landing page of your app.</p>
-      {/* Add more content here */}
-    </div>
+    <Layout>
+      <div className={styles.content}>
+      <NewIn />
+      </div>
+    </Layout>
   );
 };
 
