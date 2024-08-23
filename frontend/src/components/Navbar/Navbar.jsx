d805f52decf21516ac351bf00f49a9ce565809b0
OjElsa <elsahojwang06@gmail.com>
2024-08-23 10:58:24 +0300

    Navbar more

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,27 +1,25 @@
 import React from 'react';
 import { IconButton, Menu, MenuItem } from '@mui/material';
-import { ArrowBack, ArrowForward, MoreVert, Home } from '@mui/icons-material';
+import { ArrowBack, ArrowForward, Home } from '@mui/icons-material';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import styles from './Navbar.module.scss';
 
 const Navbar = () => {
   const [anchorEl, setAnchorEl] = React.useState(null);
-  const [profileAnchorEl, setProfileAnchorEl] = React.useState(null);
+  const [profileMenuEl, setProfileMenuEl] = React.useState(null);
 
   const handleMenuClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
 
-  const handleMenuClose = () => {
+  const handleProfileMenuClick = (event) => {
+    setProfileMenuEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
     setAnchorEl(null);
-  };
-
-  const handleProfileClick = (event) => {
-    setProfileAnchorEl(event.currentTarget);
-  };
-
-  const handleProfileClose = () => {
-    setProfileAnchorEl(null);
+    setProfileMenuEl(null);
   };
 
   const handleBack = () => {
@@ -32,49 +30,46 @@ const Navbar = () => {
     window.history.forward();
   };
 
-  const handleHomeClick = () => {
-    // Handle home navigation
-    window.location.href = '/';
-  };
-
   return (
     <nav className={styles.navbar}>
-      <div className={styles.menuSection}>
-        <IconButton onClick={handleMenuClick} className={styles.navButton}>
-          <MoreVert />
-        </IconButton>
-        <Menu
-          anchorEl={anchorEl}
-          open={Boolean(anchorEl)}
-          onClose={handleMenuClose}
-        >
-          <MenuItem onClick={handleMenuClose}>Option 1</MenuItem>
-          <MenuItem onClick={handleMenuClose}>Option 2</MenuItem>
-        </Menu>
-      </div>
-      <div className={styles.controlsSection}>
-        <IconButton onClick={handleHomeClick} className={styles.navButton}>
-          <Home />
-        </IconButton>
-        <IconButton onClick={handleBack} className={styles.navButton}>
-          <ArrowBack />
-        </IconButton>
-        <IconButton onClick={handleForward} className={styles.navButton}>
-          <ArrowForward />
-        </IconButton>
-        <IconButton onClick={handleProfileClick} className={styles.navButton}>
-          <AccountCircleIcon />
-        </IconButton>
-        <Menu
-          anchorEl={profileAnchorEl}
-          open={Boolean(profileAnchorEl)}
-          onClose={handleProfileClose}
-        >
-          <MenuItem onClick={handleProfileClose}>Profile</MenuItem>
-          <MenuItem onClick={handleProfileClose}>Settings</MenuItem>
-          <MenuItem onClick={handleProfileClose}>Logout</MenuItem>
-        </Menu>
-      </div>
+      <IconButton onClick={handleBack} className={styles.navButton}>
+        <ArrowBack />
+      </IconButton>
+      <IconButton onClick={handleForward} className={styles.navButton}>
+        <ArrowForward />
+      </IconButton>
+      
+      {/* Home Icon */}
+      <IconButton className={styles.homeButton}>
+        <Home />
+      </IconButton>
+
+      {/* Dropdown Menu */}
+      <IconButton onClick={handleMenuClick} className={styles.menuButton}>
+        <MoreVertIcon />
+      </IconButton>
+      <Menu
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Option 1</MenuItem>
+        <MenuItem onClick={handleClose}>Option 2</MenuItem>
+      </Menu>
+
+      {/* User Profile Menu */}
+      <IconButton onClick={handleProfileMenuClick} className={styles.profileMenu}>
+        <AccountCircleIcon />
+      </IconButton>
+      <Menu
+        anchorEl={profileMenuEl}
+        open={Boolean(profileMenuEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>Settings</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
     </nav>
   );
 };
