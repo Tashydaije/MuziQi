40f45ca3ee45d2f26e0a0d86b7e1d7dd8604651b
Salome-N <sallynjenga8@gmail.com>
2024-08-24 02:36:44 +0300

    Add links to App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import MainPage from './components/MainPage.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/Signin';
+import SignUp from './pages/Signup';
 
 
 function App() {
   return (
     <div className="App">
-      <MainPage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
