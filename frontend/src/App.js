a9d9326e1d6cdeaf9944bb14aefc17b356c65ca2
OjElsa <elsahojwang06@gmail.com>
2024-08-26 18:25:08 +0300

    Library and song search

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,23 @@
+// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import SignIn from './pages/Signin';
 import SignUp from './pages/Signup';
-
+import SearchResults from './pages/SearchResults/Index';
+import Library from './pages/Library/Index';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-        </Routes>
-      </Router>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/search" element={<SearchResults />} />
+        <Route path="/library" element={<Library />} />
+      </Routes>
+    </Router>
   );
 }
 
